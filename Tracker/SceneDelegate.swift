c941304a800008bd957c258b0599476c77d32df3
Igor Polunin <garry.818@yandex.ru>
2023-08-09 14:26:01 +0300

    добавлена реализация onbording с помощью pageviewContr

--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -18,40 +18,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let window = UIWindow(windowScene: scene)
         
-        let tabBarController = UITabBarController()
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistantConteiner.viewContext
-        let trackerStore = TrackerStore(context: context)
-        let trackerCategoryStore = TrackerCategoryStore(context: context,
-                                                        trackerStore: trackerStore)
-        let trackerRecordStore = TrackerRecordStore(context: context)
-        let trackerDataManager = TrackerDataManager(trackerStore: trackerStore,
-                                                    trackerCategoryStore: trackerCategoryStore,
-                                                    trackerRecordStore: trackerRecordStore,
-                                                    context: context)
-        trackerCategoryStore.setTrackerDataController(trackerDataManager.fetchResultController)
-        let mainScreenViewController = MainScreenTrackerViewController(trackerDataManager: trackerDataManager)
+//        if self.window?.rootViewController == nil {
+//                let onboardingViewController = OnboardingViewController()
+//                window.rootViewController = onboardingViewController
+//                self.window = window
+//                window.makeKeyAndVisible()
+//            }
         
-        let mainScreenNavigationController = UINavigationController(rootViewController: mainScreenViewController)
-        
-        let statisticViewController = StatisticViewController()
-
-        tabBarController.viewControllers = [mainScreenNavigationController,statisticViewController]
-        
-        //настройка вкладок таббара
-        mainScreenViewController.tabBarItem = UITabBarItem(title: "Трекеры", image: UIImage(named:"record.circle.fill"), tag: 0)
-        statisticViewController.tabBarItem = UITabBarItem(title: "Статистика", image: UIImage(named: "hare.fill"), tag: 1)
+        let tbVC = TabBarController()
+        let onboardingViewController = OnboardingViewController()
        
-       let tabBar = tabBarController.tabBar
-        tabBar.barTintColor = .Gray
-        tabBar.backgroundColor = .WhiteDay
-        let lineView = UIView(frame: CGRect(x: 0, y: 0, width: tabBar.frame.width, height: 1))
-        lineView.backgroundColor = UIColor.Gray
-        tabBar.insertSubview(lineView, at: 0)
-        window.rootViewController = tabBarController
+        window.rootViewController = onboardingViewController
         self.window = window
         window.makeKeyAndVisible()
-        
-       
     }
 }
 
